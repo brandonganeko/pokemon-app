935905e950353d6ae772366c08b571342b69d6dd
brandonganeko <bganeko@hawaii.edu>
2018-05-04 05:54:20 -1000

    Mockup
    Clock working and page design mockup

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as angular from "angular";
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -10,3 +12,22 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+var app = angular.module('clock', []);
+
+app.directive("digitalClock", function($timeout, dateFilter) {
+  return function(scope, element, attrs) {
+
+    element.addClass('alert alert-info text-center clock');
+
+    scope.updateClock = function() {
+      $timeout(function() {
+        element.text(dateFilter(new Date(), 'hh:mm:ss'));
+        scope.updateClock();
+      }, 1000);
+    };
+
+    scope.updateClock();
+
+  };
+});
